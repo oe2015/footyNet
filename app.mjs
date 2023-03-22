d61bd2d23a4aa6830c8e02e9501b285f88b0a1e9
oe2015 <oe2015@nyu.edu>
2023-03-22 11:59:45 -0400

    import

--- app.mjs
+++ app.mjs
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import 'db.mjs';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
